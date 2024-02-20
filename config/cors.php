f1901ad927acfe77fc53b3936dfde9908fa8c6c7
Thony0666 <160373228+Anthonyo02@users.noreply.github.com>
2024-02-20 07:22:35 +0300

    Update CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -1,5 +1,13 @@
 <?php
-
+    header("Access-Control-Allow-Origin: https://fianaly-delpoy-gestion-s11u.vercel.app");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+    
+    // Si la requête est une requête OPTIONS, envoyez simplement les en-têtes CORS et arrêtez l'exécution du script
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header("HTTP/1.1 200 OK");
+        exit();
+    }
 return [
 
     /*
